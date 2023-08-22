633c3f7c0c443a37253e268157b7292857d14767
Your Name <codreanu.vasile.gh@gmail.com>
2023-08-22 17:12:16 +0300

    Created sample spring boot project #1

---
+++ src/test/java/com/cedacri/logisticssystem/LogisticsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedacri.logisticssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
